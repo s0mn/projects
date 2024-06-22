6f5a1405238eef040561fc3416e986310724c3cb
kris <kristopher.clifford@gmail.com>
2024-06-22 17:56:48 -0400

    changing shit

--- timelapse/timelapse.py
+++ timelapse/timelapse.py
@@ -1,5 +1,9 @@
 from picamera2 import Picamera2, Preview
 import time
+
+timestamp = (time.strftime('%Y%m%d_%H-%M-%S'))
+
 picam2 = Picamera2()
 
+# picam2.start_and_capture_file('/opt/share/image' + 'timestamp' + '.jpg', preview_mode=None, delay=0)
 picam2.start_and_capture_file('/opt/share/image.jpg', preview_mode=None, delay=0)
