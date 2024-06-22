7808fb9df41f10109f9b884abf28bf646097b73a
kris <kristopher.clifford@gmail.com>
2024-06-22 19:05:23 -0400

    adding camera config

--- timelapse/timelapse.py
+++ timelapse/timelapse.py
@@ -4,6 +4,7 @@ import time
 timestamp = (time.strftime('%Y%m%d_%H-%M-%S'))
 
 picam2 = Picamera2()
-
-# picam2.start_and_capture_file('/opt/share/image' + 'timestamp' + '.jpg', preview_mode=None, delay=0)
-picam2.start_and_capture_file('/opt/share/image.jpg', preview_mode=None, delay=0)
+sensor_modes = picam2.sensor_modes
+picam2.resolution = (1280, 720)
+picam2.brightness = 0
+picam2.start_and_capture_file('/opt/share/image-' + timestamp + '.jpg', preview_mode=None, delay=0)
