6f28eac0434ddc35404366c1ffc1a9af2c7ab878
kris <kristopher.clifford@gmail.com>
2024-06-22 16:58:42 -0400

    updating

--- timelapse/timelapse.py
+++ timelapse/timelapse.py
@@ -1,6 +1,9 @@
-from picamera2 import PiCamera2
-from time import sleep
-
-camera = PiCamera()
-
-camera.capture('/opt/share/image.jpg')
+from picamera2 import Picamera2, Preview
+import time
+picam2 = Picamera2()
+camera_config = picam2.create_preview_configuration()
+picam2.configure(camera_config)
+picam2.start_preview(Preview.DRM)
+picam2.start()
+time.sleep(2)
+picam2.capture_file("test.jpg")
