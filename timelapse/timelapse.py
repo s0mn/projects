c9bf5d506ecc2f884cd313888cdb4bb6a0de4292
vm-user <kristopher.clifford@gmail.com>
2024-06-22 17:35:13 -0400

    working version

--- timelapse/timelapse.py
+++ timelapse/timelapse.py
@@ -2,4 +2,4 @@ from picamera2 import Picamera2, Preview
 import time
 picam2 = Picamera2()
 
-picam2.start_and_capture_file('/opt/share/image.jpg')
+picam2.start_and_capture_file('/opt/share/image.jpg', preview_mode=None, delay=0)
