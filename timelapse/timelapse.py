3cac66178721eeddbdfb6540efb640435d17687b
kris <kristopher.clifford@gmail.com>
2024-06-23 17:24:14 -0400

    auto update

--- timelapse/timelapse.py
+++ timelapse/timelapse.py
@@ -7,4 +7,4 @@ picam2 = Picamera2()
 sensor_modes = picam2.sensor_modes
 picam2.resolution = (1280, 720)
 picam2.brightness = 0
-picam2.start_and_capture_file('/opt/share/image-' + timestamp + '.jpg', preview_mode=None, delay=0)
+picam2.start_and_capture_file('/opt/share/image-' + timestamp + '.jpg', preview_mode=None, delay=0, brightness=0)
